6d49809171e0103469594dc2d4db05dc6ff66251
Nau077 <antbobrikov@yandex.ru>
2021-09-12 15:57:14 +0300

    lint-fix code

--- simple-server/server.js
+++ simple-server/server.js
@@ -4,24 +4,22 @@ const constants = require("../constants");
 const calculatePrimes = require("../heavy.calc");
 
 app.get("/", (_, res) => {
-  
-  	const startTime = new Date();
-	const result = calculatePrimes(constants.ITERATIONS, constants.MULTIPLIER);
-	
-	  const endTime = new Date();
-	  res.status(200).send({
-		...result,
-		time: endTime.getTime() - startTime.getTime() + "ms"
-	  });
- 
+  const startTime = new Date();
+  const result = calculatePrimes(constants.ITERATIONS, constants.MULTIPLIER);
+
+  const endTime = new Date();
+  res.status(200).send({
+    ...result,
+    time: endTime.getTime() - startTime.getTime() + "ms"
   });
-  
-  app.get("/testrequest", (req, res) => {
-	res.send("I am unblocked now");
-  });
-  
-  app.listen(constants.PORT, constants.HOST, () =>
-	console.log(
-	  "listening on port " + constants.PORT + " and host " + constants.HOST
-	)
-  );
+});
+
+app.get("/testrequest", (req, res) => {
+  res.send("I am unblocked now");
+});
+
+app.listen(constants.PORT, constants.HOST, () =>
+  console.log(
+    "listening on port " + constants.PORT + " and host " + constants.HOST
+  )
+);
--- worker-thread/server.js
+++ worker-thread/server.js
@@ -1,37 +1,35 @@
 const express = require("express");
 const app = express();
-const { Worker } = require("worker_threads")
+const { Worker } = require("worker_threads");
 const constants = require("../constants");
 
+app.get("/", (_, res) => {
+  const worker = new Worker("./worker.js");
 
-	app.get("/", (_, res) => {
-		const worker = new Worker("./worker.js");
-		
-		const message = {
-		  multiplier: constants.MULTIPLIER,
-		  iterations: constants.ITERATIONS
-		};
+  const message = {
+    multiplier: constants.MULTIPLIER,
+    iterations: constants.ITERATIONS
+  };
 
-		const startTime = new Date();
-		
-		worker.postMessage(message);
+  const startTime = new Date();
 
-		worker.once("message", result => {
-			const endTime = new Date();
-			res.status(200).send({
-			  ...result,
-			  time: endTime.getTime() - startTime.getTime() + "ms"
-			});
-		});
-	  
-	  });
-	  
-	  app.get("/testrequest", (req, res) => {
-		res.send("I am unblocked now");
-	  });
-	  
-	  app.listen(constants.PORT, constants.HOST, () =>
-		console.log(
-		  "listening on port " + constants.PORT + " and host " + constants.HOST
-		)
-	  );
+  worker.postMessage(message);
+
+  worker.once("message", result => {
+    const endTime = new Date();
+    res.status(200).send({
+      ...result,
+      time: endTime.getTime() - startTime.getTime() + "ms"
+    });
+  });
+});
+
+app.get("/testrequest", (req, res) => {
+  res.send("I am unblocked now");
+});
+
+app.listen(constants.PORT, constants.HOST, () =>
+  console.log(
+    "listening on port " + constants.PORT + " and host " + constants.HOST
+  )
+);
